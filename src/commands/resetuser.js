446b7c5bc58ebcb9503776646ec8875fb37f97aa
pailillomathyss-prog <pailillomathyss@gmail.com>
2026-06-12 04:17:12 +0200

    🎮 Add src/commands/resetuser.js

---
+++ src/commands/resetuser.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const db = require('../systems/database');
+const { ROLE_MILESTONES } = require('../systems/roles');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('resetuser')
+    .setDescription('[ADMIN] Remettre à zéro un joueur')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .addUserOption(opt => opt.setName('utilisateur').setDescription('Le joueur').setRequired(true)),
+
+  async execute(interaction, client) {
+    const target = interaction.options.getUser('utilisateur');
+    db.updateUser(target.id, interaction.guildId, { xp: 0, level: 0, total_messages: 0, total_voice_minutes: 0, last_message_xp: 0 });
+
+    const member = await interaction.guild.members.fetch(target.id).catch(() => null);
+    if (member) {
+      for (const milestone of ROLE_MILESTONES) {
+        const role = interaction.guild.roles.cache.find(r => r.name === milestone.name);
+        if (role) await member.roles.remove(role).catch(() => {});
+      }
+    }
+
+    await interaction.reply({ content: `✅ <@${target.id}> a été remis à zéro.`, ephemeral: true });
+  },
+};
