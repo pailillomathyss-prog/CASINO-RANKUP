0610488613d8ab15da68fe2702c076684864bb5f
pailillomathyss-prog <pailillomathyss@gmail.com>
2026-06-12 04:27:56 +0200

    📋 Add logging: src/commands/addxp.js

--- src/commands/addxp.js
+++ src/commands/addxp.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const { addXp } = require('../systems/xp');
+const { logAdminAction } = require('../systems/logger');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -13,6 +14,7 @@ module.exports = {
     const target = interaction.options.getUser('utilisateur');
     const amount = interaction.options.getInteger('quantite');
     const { newLevel, leveledUp } = await addXp(target.id, interaction.guildId, amount, client);
+    await logAdminAction(interaction.user.id, 'Ajout XP', target.id, `+${amount} XP → Niveau ${newLevel}`);
     let msg = `✅ **+${amount} XP** ajouté à <@${target.id}>. Niveau actuel : **${newLevel}**`;
     if (leveledUp) msg += ' 🎉 Level up !';
     await interaction.reply({ content: msg, ephemeral: true });
